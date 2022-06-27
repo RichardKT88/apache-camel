f8984d432bf4c3dc744bf03fa023b6fffee48b6c
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-06-27 16:14:43 -0300

    [Done] 04. Melhor legibilidade com sub-rotas

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,9 +1,9 @@
 package br.com.caelum.camel;
 
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -16,18 +16,29 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5s&noop=true").
-					setProperty("pedidoId", xpath("/pedido/id/text()")).
-					setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
-					split().
-						xpath("/pedido/itens/item").
-					filter().
-						xpath("/item/formato[text()='EBOOK']").
-					setProperty("ebookId", xpath("/item/livro/codigo/text()")).
-					log("${id} - ${body}").
-					marshal().xmljson().
-					setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
-					setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
-				to("http4://localhost:8080/webservices/ebook/item");				
+				 routeId("rota-pedidos").
+				    multicast().
+				        to("direct:soap").
+				        to("direct:http");
+				
+				from("direct:soap").
+			    	routeId("rota-soap").
+			    	log("chamando servico soap ${body}").
+		    	to("mock:soap");
+				
+				from("direct:http").
+			    routeId("rota-http").
+			    setProperty("pedidoId", xpath("/pedido/id/text()")).
+			    setProperty("email", xpath("/pedido/pagamento/email-titular/text()")).
+			    split().
+			        xpath("/pedido/itens/item").
+			    filter().
+			        xpath("/item/formato[text()='EBOOK']").
+			    setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+			    setHeader(Exchange.HTTP_QUERY,
+			            simple("clienteId=${property.email}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
+			    to("http4://localhost:8080/webservices/ebook/item");
+					
 			}
 			
 		});
