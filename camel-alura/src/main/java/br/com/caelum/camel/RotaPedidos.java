5d0ac42cb0f4420fe716bf027589faf8aac88b4d
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-06-29 16:15:58 -0300

    [Done] 06. Validação de mensagens e tratamento de erros

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -15,29 +15,38 @@ public class RotaPedidos {
 
 			@Override
 			public void configure() throws Exception {
+				errorHandler(
+					deadLetterChannel("file:erro").
+						maximumRedeliveries(3).//tente 3 vezes
+							redeliveryDelay(3000) //espera 3 segundo entre as tentativas        
+				);
 				from("file:pedidos?delay=5s&noop=true").
 				 routeId("rota-pedidos").
+				 	to("validator:pedido.xsd").
+					 
 				    multicast().
 				        to("direct:soap").
 				        to("direct:http");
 				
-				from("direct:soap").
-			    	routeId("rota-soap").
-			    	log("chamando servico soap ${body}").
-		    	to("mock:soap");
-				
 				from("direct:http").
-			    routeId("rota-http").
-			    setProperty("pedidoId", xpath("/pedido/id/text()")).
-			    setProperty("email", xpath("/pedido/pagamento/email-titular/text()")).
-			    split().
-			        xpath("/pedido/itens/item").
-			    filter().
-			        xpath("/item/formato[text()='EBOOK']").
-			    setProperty("ebookId", xpath("/item/livro/codigo/text()")).
-			    setHeader(Exchange.HTTP_QUERY,
+			    	routeId("rota-http").
+			    	setProperty("pedidoId", xpath("/pedido/id/text()")).
+			    	setProperty("email", xpath("/pedido/pagamento/email-titular/text()")).
+			    	split().
+			        	xpath("/pedido/itens/item").
+			        filter().
+			        	xpath("/item/formato[text()='EBOOK']").
+		        	setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+		        	setHeader(Exchange.HTTP_QUERY,
 			            simple("clienteId=${property.email}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
 			    to("http4://localhost:8080/webservices/ebook/item");
+				
+				from("direct:soap").
+    				routeId("rota-soap").
+				to("xslt:pedido-para-soap.xslt"). 
+					log("Resultado do Template: ${body}").
+					setHeader(Exchange.CONTENT_TYPE, constant("text/xml")).
+				to("http4://localhost:8080/webservices/financeiro");
 					
 			}
 			
